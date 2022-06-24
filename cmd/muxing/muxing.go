f44fc85639fc1286ea95fcef5cc08691d9ca173c
Evgenii Klimov <eklmv.spareacc@gmail.com>
2022-06-24 16:21:30 +0300

    implement handlers

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -20,11 +21,56 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", nameHandler).Methods("GET")
+	router.HandleFunc("/bad", badHandler).Methods("GET")
+	router.HandleFunc("/data", dataHandler).Methods("POST")
+	router.HandleFunc("/headers", headersHandler).HeadersRegexp("a", `^[+-]?\d+$`, "b", `^[+-]?\d+$`).Methods("POST")
+	router.PathPrefix("/").HandlerFunc(defaultHandler)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, %s!", vars["PARAM"])
+}
+
+func badHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "I got message:\n%s", body)
+}
+
+func headersHandler(w http.ResponseWriter, r *http.Request) {
+	a, err := strconv.Atoi(r.Header.Get("a"))
+	// should be no error because of regex?
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	b, err := strconv.Atoi(r.Header.Get("b"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("a+b", strconv.Itoa(a+b))
+	w.WriteHeader(http.StatusOK)
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
